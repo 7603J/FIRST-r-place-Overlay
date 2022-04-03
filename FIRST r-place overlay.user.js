48b9c06cdc719f6a569e339af2f33d368f22c420
7603J <67440323+7603J@users.noreply.github.com>
2022-04-03 16:44:57 -0500

    Update FIRST r-place overlay.user.js

--- FIRST r-place overlay.user.js
+++ FIRST r-place overlay.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.imgur.com/ihmK4JV.png";
+            i.src = "https://raw.githubusercontent.com/7603J/FIRST-r-place-Overlay/main/FIRST%20overlay%201.0.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
