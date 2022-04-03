420744b06c7174700b01ee54b38865ec8400e95a
7603J <67440323+7603J@users.noreply.github.com>
2022-04-03 16:39:20 -0500

    Add files via upload

---
+++ FIRST r-place overlay.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         FIRST r/place overlay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  We dont use kids to build robots we use robots to build kids
+// @author       7603
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://i.imgur.com/ihmK4JV.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
